6a7b2fa164edc067fba01628d878c9f458bac15d
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2020-03-24 10:57:43 +0100

    hämtar links och story id

--- src/DataBase.java
+++ src/DataBase.java
@@ -21,17 +21,27 @@ public class DataBase {
             int x = 1;
             String strSelect = "select body from story where id="+x;
             System.out.println("The SQL statement is: " + strSelect + "\n");
+            String linkSelect = "select story_id, target_id, description from links where id="+x;
+
 
             ResultSet rset = stmt.executeQuery(strSelect);
 
             // Loop through the result set and print
 
-            System.out.println("The records selected are:");
+
             int rowCount = 0;
             while(rset.next()) {
-                String title = rset.getString("body");
-                System.out.println(title );
+                String body = rset.getString("body");
+                System.out.println(body);
                 ++rowCount;
+
+            }
+            ResultSet link = stmt.executeQuery(linkSelect);
+            while(link.next()){
+                String storyId = link.getString("story_id");
+                String targetId = link.getString("target_id");
+                String description = link.getString("description");
+                System.out.println(storyId+" "+targetId+" "+description);
             }
             System.out.println("Total number of records = " + rowCount);
 
